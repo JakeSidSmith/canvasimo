58ab18e14762c11ad3a7853d2d9f9287ce893f7e
Jake 'Sid' Smith <jake@dabapps.com>
2016-04-30 16:06:56 +0100

    Initial commit

---
+++ examples/demo-only-stuff/create-canvas.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function () {
+
+  function createCanvas () {
+    var canvases = document.getElementsByTagName('canvas');
+    var latestCanvas = canvases[canvases.length - 1];
+
+    if (latestCanvas) {
+      latestCanvas.id = '';
+    }
+
+    var scripts = document.getElementsByTagName('script');
+    var latestScript = scripts[scripts.length - 1];
+
+    var newCanvas = document.createElement('canvas');
+    newCanvas.id = 'canvas';
+
+    latestScript.parentNode.insertBefore(newCanvas, latestScript);
+  }
+
+  window.createCanvas = createCanvas;
+
+})();
---
+++ tests/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('an example test suite', function () {
+
+  it('should pass', function () {
+    expect(true).to.be.true;
+  });
+
+});
