232b31c6ea65da0237fcfe7047bc1e0eb477ac47
Jake 'Sid' Smith <jake@dabapps.com>
2016-05-01 16:22:44 +0100

    Test some getters

--- tests/helpers/get-context-stub.js
+++ tests/helpers/get-context-stub.js
@@ -2,51 +2,70 @@
 
 (function () {
 
+  var ctx = {
+    imageSmoothingEnabled: true,
+    webkitImageSmoothingEnabled: false,
+    globalAlpha: 1, // number
+    globalCompositeOperation: 'source-over', // string
+    fillStyle: '#000000', // string
+    strokeStyle: '#000000', // string
+    lineWidth: 1, // number
+    lineCap: 'butt', // string
+    lineJoin: 'miter', // string
+    lineDashOffset: 0, // number
+    miterLimit: 10, // number
+    shadowColor: 'rgba(0, 0, 0, 0)', // string
+    shadowBlur: 0, // number
+    shadowOffsetX: 0, // number
+    shadowOffsetY: 0, // number
+    font: '10px sans-serif', // string
+    textAlign: 'start', // string
+    textBaseline: 'alphabetic', // string
+    getContextAttributes: function () {},
+    getImageData: function () {},
+    createLinearGradient: function () {},
+    createRadialGradient: function () {},
+    createPattern: function () {},
+    createImageData: function () {},
+    isPointInPath: function () {},
+    isPointInStroke: function () {},
+    measureText: function () {},
+    getLineDash: function () {},
+    save: function () {},
+    restore: function () {},
+    scale: function () {},
+    rotate: function () {},
+    translate: function () {},
+    transform: function () {},
+    setTransform: function () {},
+    resetTransform: function () {},
+    fill: function () {},
+    stroke: function () {},
+    drawFocusIfNeeded: function () {},
+    clip: function () {},
+    clearRect: function () {},
+    moveTo: function () {},
+    lineTo: function () {},
+    quadraticCurveTo: function () {},
+    bezierCurveTo: function () {},
+    arcTo: function () {},
+    beginPath: function () {},
+    fillText: function () {},
+    strokeText: function () {},
+    drawImage: function () {},
+    putImageData: function () {},
+    closePath: function () {},
+    rect: function () {},
+    arc: function () {},
+    ellipse: function () {},
+    setLineDash: function () {},
+    fillRect: function () {},
+    strokeRect: function () {}
+  };
+
   module.exports = function getContext (type) {
     if (type === '2d') {
-      return {
-        getContextAttributes: function () {},
-        getImageData: function () {},
-        createLinearGradient: function () {},
-        createRadialGradient: function () {},
-        createPattern: function () {},
-        createImageData: function () {},
-        isPointInPath: function () {},
-        isPointInStroke: function () {},
-        measureText: function () {},
-        getLineDash: function () {},
-        save: function () {},
-        restore: function () {},
-        scale: function () {},
-        rotate: function () {},
-        translate: function () {},
-        transform: function () {},
-        setTransform: function () {},
-        resetTransform: function () {},
-        fill: function () {},
-        stroke: function () {},
-        drawFocusIfNeeded: function () {},
-        clip: function () {},
-        clearRect: function () {},
-        moveTo: function () {},
-        lineTo: function () {},
-        quadraticCurveTo: function () {},
-        bezierCurveTo: function () {},
-        arcTo: function () {},
-        beginPath: function () {},
-        fillText: function () {},
-        strokeText: function () {},
-        drawImage: function () {},
-        putImageData: function () {},
-        closePath: function () {},
-        rect: function () {},
-        arc: function () {},
-        ellipse: function () {},
-        setLineDash: function () {},
-        fillRect: function () {},
-        strokeRect: function () {},
-        imageSmoothingEnabled: true
-      };
+      return ctx;
     }
     throw new Error('Context must be "2d"');
   };
--- tests/index.js
+++ tests/index.js
@@ -17,6 +17,54 @@ describe('sensible canvas', function () {
     };
   }
 
+  var getters = {
+    getOpacity: {value: 1, type: 'number'},
+    getGlobalCompositeOperation: {value: 'source-over', type: 'string'},
+    getFill: {value: '#000000', type: 'string'},
+    getStroke: {value: '#000000', type: 'string'},
+    getStrokeWidth: {value: 1, type: 'number'},
+    getStrokeCap: {value: 'butt', type: 'string'},
+    getStrokeJoin: {value: 'miter', type: 'string'},
+    getStrokeDashOffset: {value: 0, type: 'number'},
+    getMiterLimit: {value: 10, type: 'number'},
+    getShadowColor: {value: 'rgba(0, 0, 0, 0)', type: 'string'},
+    getShadowBlur: {value: 0, type: 'number'},
+    getShadowOffsetX: {value: 0, type: 'number'},
+    getShadowOffsetY: {value: 0, type: 'number'},
+    getFont: {value: '10px sans-serif', type: 'string'},
+    getTextAlign: {value: 'start', type: 'string'},
+    getTextBaseline: {value: 'alphabetic', type: 'string'}
+  };
+
+  var argumentMap = {
+    setWidth: [0],
+    setHeight: [0],
+    setSize: [0, 0],
+    scale: [1, 1],
+    rotate: [0],
+    translate: [0, 0],
+    setTransform: [1, 0, 0, 1, 0, 0],
+    transform: [1, 0, 0, 1, 0, 0],
+    drawFocusIfNeeded: [element],
+    clearRect: [0, 0, 0, 0],
+    moveTo: [0, 0],
+    lineTo: [0, 0],
+    quadraticCurveTo: [0, 0, 0, 0],
+    bezierCurveTo: [0, 0, 0, 0, 0, 0],
+    arcTo: [0, 0, 0, 0, 0],
+    fillText: ['', 0, 0],
+    strokeText: ['', 0, 0],
+    drawImage: [new Image(), 0, 0],
+    putImageData: [new ImageData(1, 1), 0, 0],
+    plotRect: [0, 0, 0, 0],
+    plotArc: [0, 0, 0, 0, 0],
+    plotEllipse: [0, 0, 0, 0, 0, 0, 0],
+    setStrokeDash: [[]],
+    isPointInPath: [0, 0]
+  };
+
+  var isGetter = /^(get|create|is)/i;
+
   it('should return an interface', function () {
     element = document.createElement('canvas');
     stub(element, 'getContext', getContextStub);
@@ -25,38 +73,47 @@ describe('sensible canvas', function () {
     expect(canvas).to.exist;
   });
 
-  describe('interface', function () {
+  describe('property getters', function () {
 
-    it('should return itself for every method except getters', function () {
-      var argumentMap = {
-        setWidth: [0],
-        setHeight: [0],
-        setSize: [0, 0],
-        scale: [1, 1],
-        rotate: [0],
-        translate: [0, 0],
-        setTransform: [1, 0, 0, 1, 0, 0],
-        transform: [1, 0, 0, 1, 0, 0],
-        drawFocusIfNeeded: [element],
-        clearRect: [0, 0, 0, 0],
-        moveTo: [0, 0],
-        lineTo: [0, 0],
-        quadraticCurveTo: [0, 0, 0, 0],
-        bezierCurveTo: [0, 0, 0, 0, 0, 0],
-        arcTo: [0, 0, 0, 0, 0],
-        fillText: ['', 0, 0],
-        strokeText: ['', 0, 0],
-        drawImage: [new Image(), 0, 0],
-        putImageData: [new ImageData(1, 1), 0, 0],
-        plotRect: [0, 0, 0, 0],
-        plotArc: [0, 0, 0, 0, 0],
-        plotEllipse: [0, 0, 0, 0, 0, 0, 0],
-        setStrokeDash: [[]],
-        isPointInPath: [0, 0]
-      };
+    it('should return the correct property values from the context', function () {
+      for (var key in getters) {
+        var result = canvas[key]();
+        expect(result).to.equal(getters[key].value);
+        expect(typeof result).to.equal(getters[key].type);
+      }
+    });
 
-      var isGetter = /^(get|create|is)/i;
+  });
 
+  describe('image smoothing', function () {
+
+    it('should return the first image smoothing value', function () {
+      expect(canvas.getImageSmoothing()).to.be.true;
+    });
+
+    it('should set the first image smoothing value', function () {
+      canvas.setImageSmoothing(false);
+
+      expect(canvas.getImageSmoothing()).to.be.false;
+    });
+
+  });
+
+  describe('context getters', function () {
+
+    it('should return the actual canvas context', function () {
+      expect(canvas.getContext()).to.equal(element.getContext('2d'));
+    });
+
+    it('should return the context type', function () {
+      expect(canvas.getContextType()).to.equal('2d');
+    });
+
+  });
+
+  describe('actions and setters', function () {
+
+    it('should return the canvas', function () {
       for (var key in canvas) {
         if (!isGetter.exec(key)) {
           expect(canvas[key].apply(null, argumentMap[key])).to.equal(canvas);
