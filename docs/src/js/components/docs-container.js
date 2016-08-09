fd7ddeafe0ba1404c5289bd9ab3772d19b34c574
Jake 'Sid' Smith <jake@dabapps.com>
2016-08-09 22:26:59 +0100

    Begin generating docs with React

---
+++ docs/src/js/components/docs-container.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function () {
+
+  var React = require('react');
+  // var docs = require('./docs');
+
+  var DocsContainer = React.createClass({
+    render: function () {
+      return (
+        <div>
+          Hello, World!
+        </div>
+      );
+    }
+  });
+
+  module.exports = DocsContainer;
+
+})();
