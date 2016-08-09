924149c094e0976de1c5c50044bc1f26e581cbe5
Jake 'Sid' Smith <jake@dabapps.com>
2016-08-09 22:33:04 +0100

    Begin looping over groups

--- docs/src/js/components/docs-container.js
+++ docs/src/js/components/docs-container.js
@@ -3,12 +3,25 @@
 (function () {
 
   var React = require('react');
-  // var docs = require('./docs');
+  var docs = require('../docs');
 
   var DocsContainer = React.createClass({
     render: function () {
       return (
-        <div>
+        <div className="container">
+          <div className="doc-container">
+            {
+              docs.map(function (group) {
+                return (
+                  <div className="group" key={group.name}>
+                    <h2>
+                      {group.name}
+                    </h2>
+                  </div>
+                );
+              })
+            }
+          </div>
           Hello, World!
         </div>
       );
