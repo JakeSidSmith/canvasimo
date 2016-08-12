5f16b7167d93a7d1f19acb4456f7a38d7ca45f33
Jake 'Sid' Smith <jake@dabapps.com>
2016-08-12 12:44:57 +0100

    Add about section to docs

--- docs/src/js/components/document.js
+++ docs/src/js/components/document.js
@@ -36,6 +36,35 @@
                   </span>
                 </h1>
 
+                <p>
+                  Canvasimo (Canvas in my opinion) started off as a simple concept - create wrappers for the standard canvas API to create a fluent interface, and allow access to canvas attributes with getters and setters.
+                </p>
+
+                <p>
+                  This quickly evolved into a project that not only wrapped the existing canvas API, but added some additional methods (e.g. for creating rounded rectangles), and ensured that experimental features (such as reset transform and ellipse) worked in all browsers.
+                </p>
+
+                <p>
+                  Along the way, I realised that a lot of the canvas attributes & methods were not very idiomatic, and so I came up with some more suitable method / attribute names.
+                </p>
+
+                <p>
+                  For example, shape drawing is broken into 3 types: <code>plotShape</code>, <code>fillShape</code>, and <code>strokeShape</code>.
+                  You'll find these 3 methods for almost every shape, e.g. <code>plotRect</code>, <code>fillRect</code> and <code>strokeRect</code>.
+                  All the original methods are still available as aliases, e.g. <code>rect</code>.
+                </p>
+
+                <p>
+                  Similarly attributes regarding lines and strokes had a mixed set of names, so to simplify this, any style related attributes are referred to as stroke, and line is now a shape.
+                  For example, where you previously had <code>strokeStyle</code> and <code>lineWidth</code>, you now have <code>stroke</code> and <code>strokeWidth</code>, both of which are available through getters and setters; <code>setStroke</code>, <code>setStrokeWidth</code>.
+                  You can, however, still use their aliases; <code>setStrokeStyle</code>, <code>setLineWidth</code>.
+                </p>
+
+                <p>
+                  Additionally, any methods that previously relied on setting the stroke or fill color before-hand, can now optionally have a color passed as their final argument, e.g. <code>fillRect(0, 0, 10, 10, 'black')</code>, <code>strokeLine(0, 0, 0, 20, 'red')</code>.
+                  And similarly, the fill and stroke methods can now be passed a color; <code>fill('red')</code>, <code>stroke('green')</code>, <code>fillCanvas('blue')</code>.
+                </p>
+
                 {
                   false && (
                     <div>
