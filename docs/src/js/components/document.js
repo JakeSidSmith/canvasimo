daaaafbef579ff8803ca4f730436b7098c27235c
Jake 'Sid' Smith <jake@dabapps.com>
2016-08-24 14:31:16 +0100

    Author meta

--- docs/src/js/components/document.js
+++ docs/src/js/components/document.js
@@ -15,6 +15,7 @@
           <head>
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+            <meta name="author" content="Jake 'Sid' Smith" />
             <title>
               Canvasimo
             </title>
