52de8c6f74d82b9a4d71e438c99e85521be87cfd
Jake 'Sid' Smith <jake@dabapps.com>
2016-08-27 16:39:21 +0100

    Use double quotes in script

--- docs/src/js/components/analytics.js
+++ docs/src/js/components/analytics.js
@@ -4,12 +4,12 @@
 
   var React = require('react');
 
-  var analyticsScript = '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){' +
+  var analyticsScript = '(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){' +
     '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),' +
     'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)' +
-    '})(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');' +
-    'ga(\'create\', \'UA-64450504-3\', \'auto\');' +
-    'ga(\'send\', \'pageview\');';
+    '})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");' +
+    'ga("create", "UA-64450504-3", "auto");' +
+    'ga("send", "pageview");';
 
   var Analytics = React.createClass({
     render: function () {
