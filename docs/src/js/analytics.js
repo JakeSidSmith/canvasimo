85c71db75168af25ad07fb7d7262680d72d8e1a5
Jake 'Sid' Smith <jake@dabapps.com>
2016-08-27 16:12:37 +0100

    Disable eslint on analytics script

--- docs/src/js/analytics.js
+++ docs/src/js/analytics.js
@@ -2,6 +2,7 @@
 
 (function () {
 
+  /* eslint-disable */
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -9,5 +10,6 @@
 
   ga('create', 'UA-64450504-3', 'auto');
   ga('send', 'pageview');
+  /* eslint-enable */
 
 })();
