c92599f801b00808678afcd797381a0f9fa00b2b
Jake 'Sid' Smith <jake@dabapps.com>
2018-01-29 23:58:19 +0000

    Convert get bounding client rect stub

---
+++ tests/helpers/get-bounding-client-rect-stub.ts
@@ -0,0 +1,12 @@
+const getBoundingClientRect = () => {
+  return {
+    top: 0,
+    left: 0,
+    right: 50,
+    bottom: 50,
+    width: 100,
+    height: 100,
+  };
+};
+
+export default getBoundingClientRect;
