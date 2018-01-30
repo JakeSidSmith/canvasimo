5e434fd99783d24bfbda368c7a33e4c73018df0d
Jake 'Sid' Smith <jake@dabapps.com>
2018-01-30 00:08:24 +0000

    Fix linting

--- tests/helpers/utils.ts
+++ tests/helpers/utils.ts
@@ -1,22 +1,22 @@
 interface Each {
-  (obj: any[], callback: (value: any, index: number) => any): void;
+  (obj: any[] | string, callback: (value: any, index: number) => any): void;
   (obj: {[i: string]: any}, callback: (value: any, key: string) => any): void;
 }
 
 interface Some {
-  (obj: any[], callback: (value: any, index: number) => any): void;
+  (obj: any[] | string, callback: (value: any, index: number) => any): void;
   (obj: {[i: string]: any}, callback: (value: any, key: string) => any): void;
 }
 
-export const each = (
-  obj: any[] | {[i: string]: any},
+export const each: Each = (
+  obj: any[] | string | {[i: string]: any},
   callback: (value: any, key: number | string) => any
 ): void => {
   if (Array.isArray(obj) || typeof obj === 'string') {
     for (let i = 0; i < obj.length; i += 1) {
       callback(obj[i], i);
     }
-  } else if (typeof obj === 'object') {
+  } else {
     for (const key in obj) {
       if (Object.prototype.hasOwnProperty.call(obj, key)) {
         callback(obj[key], key);
@@ -26,7 +26,7 @@ export const each = (
 };
 
 export const some: Some = (
-  obj: any[] | {[i: string]: any},
+  obj: any[] | string | {[i: string]: any},
   callback: (value: any, key: number | string
 ) => any): boolean => {
   if (Array.isArray(obj) || typeof obj === 'string') {
@@ -35,7 +35,7 @@ export const some: Some = (
         return true;
       }
     }
-  } else if (typeof obj === 'object') {
+  } else {
     for (const key in obj) {
       if (Object.prototype.hasOwnProperty.call(obj, key)) {
         if (callback(obj[key], key)) {
