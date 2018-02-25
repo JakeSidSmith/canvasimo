cf09ee5e83c63d86c83f5c24f60b528e03ce7540
Jake 'Sid' Smith <jake@dabapps.com>
2018-02-25 17:04:24 +0000

    Load example code with fs

--- docs/src/ts/components/document.tsx
+++ docs/src/ts/components/document.tsx
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 import * as React from 'react';
 import { Component } from 'react';
 import { Docs } from '../types';
@@ -6,8 +8,15 @@ import Group from './group';
 import LinkHeader from './link-header';
 import Sidebar from './sidebar';
 
+const MATCHES_CANVASIMO_IMPORT = /import\s+Canvasimo\s+from\s+'.+'/;
+const MATCHES_CANVASIMO_PATH = /'.+'/;
+
 // tslint:disable-next-line:no-var-requires
 const docs: Docs = require('../../../build/json/docs.json');
+const example = fs.readFileSync(path.join(__dirname, '../example.ts'), 'utf8')
+  .replace(MATCHES_CANVASIMO_IMPORT, (subString: string) => {
+    return subString.replace(MATCHES_CANVASIMO_PATH, '\'canvasimo\'');
+  });
 
 const TITLE = 'Canvasimo | The fluent HTML5 canvas drawing library';
 
@@ -210,56 +219,7 @@ export default class Document extends Component<Props, {}> {
                   You must have javascript enabled for this example.
                 </noscript>
 
-                <pre>{`const element = document.getElementById('canvas');
-const canvas = new Canvasimo(element);
-const rect = canvas.getBoundingClientRect();
-
-const randoms = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => Math.random());
-
-canvas
-  .setDensity(2)
-  .setSize(rect.width, rect.height);
-
-const draw = () => {
-  const { width } = canvas.getBoundingClientRect();
-  const height = canvas.getHeight();
-
-  canvas
-    .clearCanvas()
-    .setSize(width, height)
-    .fillCanvas('#EEEEEE')
-    .save()
-    .translate(width / 2, height / 2)
-    .rotate(canvas.getRadiansFromDegrees(-90))
-    .setOpacity(0.25)
-    .fillStar(0, 0, 50, 5, false, 'red')
-    .restore()
-    .strokeCircle(width * 0.25, height / 2, 30, false, 'black')
-    .strokeBurst(width * 0.25, height / 2, 40, 50, 8, false, 'black')
-    .strokeRoundedRect(width * 0.75 - 20, height / 2 - 20, 40, 40, 10, 'black')
-    .repeat(10, (index) => {
-      const y = canvas.map(index, 0, 10, 0, height) + height / 10 / 2;
-
-      canvas
-        .beginPath()
-        .strokeLine(10, y, width - 10, y, 'green');
-    })
-    .tap(() => {
-      const path = randoms.map((value, index) => ({
-        x: canvas.getFractionOfWidth(1 / (randoms.length - 1)) * index,
-        y: canvas.map(value, 0, 1, 0, height),
-      }));
-
-      canvas
-        .beginPath()
-        .setStrokeWidth(2)
-        .strokePath(path, 'blue');
-    });
-};
-
-draw();
-
-window.addEventListener('resize', draw);`}</pre>
+                <pre>{example}</pre>
 
                 <LinkHeader type="h1" header="Documentation" className="main-header" />
 
