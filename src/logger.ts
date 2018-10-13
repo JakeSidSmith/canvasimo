e8515f5835909162a34cfbb9bc59ea9baedaf74d
Jake 'Sid' Smith <jake@dabapps.com>
2018-10-13 16:17:55 +0100

    Check for window.console and use this for logs

--- src/logger.ts
+++ src/logger.ts
@@ -1,18 +1,14 @@
+const consoleExists = Boolean(window.console);
+
 const logger = {
   info: (message: string) => {
-    // tslint:disable-next-line:no-console
-    // tslint:disable-next-line:strict-type-predicates
-    if (console && typeof console.info === 'function') {
-      // tslint:disable-next-line:no-console
-      console.info(`Canvasimo: ${message}`);
+    if (consoleExists) {
+      window.console.info(`Canvasimo: ${message}`);
     }
   },
   warn: (message: string) => {
-    // tslint:disable-next-line:no-console
-    // tslint:disable-next-line:strict-type-predicates
-    if (console && typeof console.warn === 'function') {
-      // tslint:disable-next-line:no-console
-      console.warn(`Canvasimo: ${message}`);
+    if (consoleExists) {
+      window.console.warn(`Canvasimo: ${message}`);
     }
   },
 };
