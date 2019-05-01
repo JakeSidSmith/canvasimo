019d0518ebcc3b544a761486651af3958ebeccd0
Jake 'Sid' Smith <jake@dabapps.com>
2019-05-01 22:45:33 +0100

    Add regression test for resetting density related values

--- tests/density.ts
+++ tests/density.ts
@@ -5,12 +5,14 @@ describe('density', () => {
 
   const element = document.createElement('canvas');
   jest.spyOn(element, 'getContext').mockImplementation(getContextStub);
-  const instance = new Canvasimo(element);
+  const instance = new Canvasimo(element).setWidth(100);
   const ctx = instance.getCurrentContext();
 
   it('should explicitly set the font size upon creation', () => {
     expect(ctx.font).toBe('normal normal normal 10px sans-serif');
     expect(instance.getFontSize()).toBe(10);
+    expect(element.width).toBe(100);
+    expect(instance.getWidth()).toBe(100);
   });
 
   it('updates font size when changing the density', () => {
@@ -18,14 +20,18 @@ describe('density', () => {
 
     expect(ctx.font).toBe('normal normal normal 20px sans-serif');
     expect(instance.getFontSize()).toBe(10);
+    expect(element.width).toBe(200);
+    expect(instance.getWidth()).toBe(100);
   });
 
-  it('retains density when the canvas is cleared', () => {
+  it('retains density when the canvas is cleared (without changing the canvas size)', () => {
     // Fake browser font reset
     ctx.font = '10px sans-serif';
     instance.clearCanvas();
 
     expect(ctx.font).toBe('normal normal normal 20px sans-serif');
     expect(instance.getFontSize()).toBe(10);
+    expect(element.width).toBe(200);
+    expect(instance.getWidth()).toBe(100);
   });
 });
