ca18dfe1329da51efee25d8aca367d1f412258db
Jake 'Sid' Smith <jake@dabapps.com>
2019-05-05 16:38:42 +0100

    Create default context values constant

--- src/constants.ts
+++ src/constants.ts
@@ -39,6 +39,7 @@ export const INCORRECT_GET_ANGLE_ARGUMENTS = 'Incorrect number of arguments supp
  'Arguments must be [x1, y1, x2, y2] or [x1, y1, x2, y2, x3, y3].';
 
 export const DEFAULT_FONT_PARTS = ['normal', 'normal', 'normal', '10px', 'sans-serif'];
+export const DEFAULT_FONT = DEFAULT_FONT_PARTS.join(' ');
 export const DEFAULT_DENSITY = 1;
 
 export const MATCHES_SPECIAL_FILL = /^(nonzero|evenodd)$/i;
@@ -51,3 +52,25 @@ export const MATCHES_WHITESPACE = /\s+/g;
 export const MATCHES_ALL_WHITESPACE = /^\s*$/;
 export const MATCHES_FONT_SIZE = /(^|\s+)(\d*\.?\d+)([a-z]+|%)(\/\d*\.?\d+(?:[a-z]+|%)?)?\s/i;
 export const MATCHES_WORD_BREAKS = /(?![^\w\s])\b/g;
+
+export const DEFAULT_CONTEXT_VALUES = {
+  globalAlpha: 1,
+  globalCompositeOperation: 'source-over',
+  filter: 'none',
+  imageSmoothingEnabled: true,
+  imageSmoothingQuality: 'low',
+  strokeStyle: '#000000',
+  fillStyle: '#000000',
+  shadowOffsetX: 0,
+  shadowOffsetY: 0,
+  shadowBlur: 0,
+  shadowColor: 'rgba(0, 0, 0, 0)',
+  lineWidth: 1,
+  lineCap: 'butt',
+  lineJoin: 'miter',
+  miterLimit: 10,
+  lineDashOffset: 0,
+  font: DEFAULT_FONT,
+  textAlign: 'start',
+  textBaseline: 'alphabetic',
+};
